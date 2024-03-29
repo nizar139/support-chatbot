2fbc2f4ab8b1ce8d3af8d86417b2b3946e28d05c
nizaz139 <nizarelghazal@gmail.com>
2024-03-29 22:48:00 +0100

    update config

--- chatbot/config.py
+++ chatbot/config.py
@@ -2,12 +2,13 @@ import os
 
 # For API keys, you can either add them in the environment variables, or add a default value for os.getenv
 
-OPENAI_KEY = os.getenv("OPENAI_KEY", "")
-HUGGINFACEHUB_API_TOKEN = os.getenv("HUGGINFACEHUB_API_TOKEN", "")
+OPENAI_KEY = os.getenv("OPENAI_KEY", "your key")                               
+HUGGINFACEHUB_API_TOKEN = os.getenv("HUGGINFACEHUB_API_TOKEN", "your token")    
 
 
-# LLM_PROVIDER = "openai"
-LLM_PROVIDER = "huggingface"
+providers = ["openai","huggingface"]
+
+LLM_PROVIDER = "huggingface" # use the provider that you have an API key for from the providers list
 
 # repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
